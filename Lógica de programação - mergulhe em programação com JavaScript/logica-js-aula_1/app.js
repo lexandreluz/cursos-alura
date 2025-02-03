c51ec82ff0484b5ea2f867c539ccd929702b528b
Alexandre Luz <43273863+lexandreluz@users.noreply.github.com>
2025-02-03 11:39:24 +0000

    exercícios

--- Lógica de programação - mergulhe em programação com JavaScript/logica-js-aula_1/app.js
+++ Lógica de programação - mergulhe em programação com JavaScript/logica-js-aula_1/app.js
@@ -1,7 +1,14 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 5;
+let numeroSecreto = 2;
+console.log(numeroSecreto);
 let chute = prompt('Escolha um número entre 1 e 10');
 
 if (chute == numeroSecreto) {
-    console.log('Isso ai! Você descobriu o número secreto (5)');
+    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
+} else {
+    if (chute > numeroSecreto){
+        alert(`O número secreto é menor que ${chute}`);
+    } else {
+        alert(`O número secreto é maior que ${chute}`)
+    }
 }
--- Lógica de programação - mergulhe em programação com JavaScript/logica-js-projeto_inicial/app.js
+++
@@ -1,7 +0,0 @@
-alert('Bem vindo ao jogo do número secreto!');
-let numeroSecreto = 2;
-let chute = prompt('Escolha um número entre 1 e 30');
-
-if(numeroSecreto == chute) {
-  console.log('Parabéns, você descobriu o número secreto!');
-}
