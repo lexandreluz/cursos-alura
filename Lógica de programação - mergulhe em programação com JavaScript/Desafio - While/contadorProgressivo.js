126b4567be09a57897be3ba8b31866d722d78cfd
Alexandre Luz <43273863+lexandreluz@users.noreply.github.com>
2025-02-04 14:35:28 -0300

    Add files via upload

---
+++ Lógica de programação - mergulhe em programação com JavaScript/Desafio - While/contadorProgressivo.js
@@ -0,0 +1,6 @@
+let contador = 0;
+limite = prompt('Insira o número para contagem progressiva: ');
+while (contador <= limite){
+    console.log(contador);
+    contador++;
+}
---
+++ Lógica de programação - mergulhe em programação com JavaScript/Desafio - While/contadorRegressivo.js
@@ -0,0 +1,5 @@
+limite = prompt('Insira o número para contagem regressiva: ');
+while (limite >= 0){
+    console.log(limite);
+    limite--;
+}
