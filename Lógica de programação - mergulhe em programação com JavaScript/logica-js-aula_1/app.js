8c9449fb74d752946cd61c76b76db664b2b547bb
Alexandre Luz <43273863+lexandreluz@users.noreply.github.com>
2025-02-04 15:05:13 -0300

    Update app.js

--- Lógica de programação - mergulhe em programação com JavaScript/logica-js-aula_1/app.js
+++ Lógica de programação - mergulhe em programação com JavaScript/logica-js-aula_1/app.js
@@ -1,26 +1,29 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = parseInt(Math.random() * 100 + 1);
-console.log(numeroSecreto)
+let numeroMaximo = 2000
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
+console.log(numeroSecreto);
 let chute;
 let tentativas = 1;
 
 // enquanto chute não for igual ao n.s.
 while (chute != numeroSecreto) {
-    chute = prompt('Escolha um número entre 1 e 100');
+    chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
     // se chute for igual ao número secreto
     if (chute == numeroSecreto) {
-       break;
+        break;
     } else {
         if (chute > numeroSecreto) {
-            alert(`O número secreto é menor que ${chute}`)
+            alert(`O número secreto é menor que ${chute}`);
         } else {
-            alert(`O número secreto é maior que ${chute}`)
+            alert(`O número secreto é maior que ${chute}`);
         }
-        // tentativas = tentativas + 1
-        tentativas++
+        // tentativas = tentativas + 1;
+        tentativas++;
     }
 }
 
-let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa'
-alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}`);
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
+
+
 
